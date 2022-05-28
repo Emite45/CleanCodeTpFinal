dc32a9d5549c80d642dfcda87078169a164576c3
Aymeric L <thesuperspeed45@gmail>
2022-05-28 21:24:58 +0200

    Nettoyage du code et maj des tests

--- src/billing/BillingTest.java
+++ src/billing/BillingTest.java
@@ -9,49 +9,49 @@ class BillingTest {
 	@Test
 	void whenProductIsNotImportedNorFoodNorMedicineNorBookThenTaxIsTenPercent() {
 		Product myProduct = new Product("flacon de parfum", 10.0, false, "Beauty");
-		assertEquals(0.1 * myProduct.getPrice(), TaxSystem.TAXABLE.getTax(myProduct));
+		assertEquals(0.1 * myProduct.getPrice(), new TaxCalculatorClient().calculateTax(myProduct));
 	}
 	
 	@Test
 	void whenProductIsImportedAndNotFoodNorMedicineNorBookThenTaxIsFifteenPercent() {
 		Product myProduct = new Product("flacon de parfum", 10.0, true, "Beauty");
-		assertEquals(0.15 * myProduct.getPrice(), TaxSystem.IMPORTED_TAXABLE.getTax(myProduct));
+		assertEquals(0.15 * myProduct.getPrice(), new TaxCalculatorClient().calculateTax(myProduct));
 	}
 	
 	@Test
 	void whenProductIsFoodNotImportedThenTaxIsZero() {
 		Product myProduct = new Product("pizza", 10.0, false, "Food");
-		assertEquals(0, TaxSystem.NON_TAXABLE.getTax(myProduct));
+		assertEquals(0, new TaxCalculatorClient().calculateTax(myProduct));
 	}
 	
 	@Test
 	void whenProductIsMedicineNotImportedThenTaxIsZero() {
 		Product myProduct = new Product("doliprane", 10.0, false, "Medicine");
-		assertEquals(0, TaxSystem.NON_TAXABLE.getTax(myProduct));
+		assertEquals(0, new TaxCalculatorClient().calculateTax(myProduct));
 	}
 	
 	@Test
 	void whenProductIsBookNotImportedThenTaxIsZero() {
 		Product myProduct = new Product("Le Clean Code pour les nuls", 10.0, false, "Book");
-		assertEquals(0, TaxSystem.NON_TAXABLE.getTax(myProduct));
+		assertEquals(0, new TaxCalculatorClient().calculateTax(myProduct));
 	}
 	
 	@Test
 	void whenProductIsImportedFoodThenTaxIsFivePercent() {
 		Product myProduct = new Product("pizza", 10.0, true, "Food");
-		assertEquals(0.05 * myProduct.getPrice(), TaxSystem.IMPORTED_NON_TAXABLE.getTax(myProduct));
+		assertEquals(0.05 * myProduct.getPrice(), new TaxCalculatorClient().calculateTax(myProduct));
 	}
 	
 	@Test
 	void whenProductIsImportedMedicineThenTaxIsFivePercent() {
 		Product myProduct = new Product("doliprane", 10.0, true, "Medicine");
-		assertEquals(0.05 * myProduct.getPrice(), TaxSystem.IMPORTED_NON_TAXABLE.getTax(myProduct));
+		assertEquals(0.05 * myProduct.getPrice(), new TaxCalculatorClient().calculateTax(myProduct));
 	}
 	
 	@Test
-	void whenProductIsBookImportedBookThenTaxIsFivePercent() {
+	void whenProductIsImportedBookThenTaxIsFivePercent() {
 		Product myProduct = new Product("Le Clean Code pour les nuls", 10.0, true, "Book");
-		assertEquals(0.05 * myProduct.getPrice(), TaxSystem.IMPORTED_NON_TAXABLE.getTax(myProduct));
+		assertEquals(0.05 * myProduct.getPrice(), new TaxCalculatorClient().calculateTax(myProduct));
 	}
 
 }
--- src/billing/TaxCalculatorClient.java
+++ src/billing/TaxCalculatorClient.java
@@ -2,17 +2,20 @@ package billing;
 
 public class TaxCalculatorClient {
 
-	public TaxSystem getAppropriateTaxSystem(Product myProduct) {
+	public double calculateTax(Product product) {
+		return getAppropriateTaxSystem(product).getTax(product);
+	}
+	
+	TaxSystem getAppropriateTaxSystem(Product myProduct) {
 		
-		if(productIsImported(myProduct)) {
-			return TaxSystem.IMPORTED_TAXABLE;
-		}
 		if(productHasNoVAT(myProduct)) {
-			
 			if(productIsImported(myProduct)) {
-				return TaxSystem.IMPORTED_TAXABLE;
+				return TaxSystem.IMPORTED_NON_TAXABLE;
 			}
 			return TaxSystem.NON_TAXABLE;
+		}
+		if(productIsImported(myProduct)) {
+			return TaxSystem.IMPORTED_TAXABLE;
 		}
 		return TaxSystem.TAXABLE;
 	}
