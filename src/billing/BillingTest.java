5daddaa27d818c01d0a2570a65a0f54d8044ed03
judnl <65891291+judnl@users.noreply.github.com>
2022-05-29 16:54:48 +0200

    rounded tax for product (not total products)

--- src/billing/BillingTest.java
+++ src/billing/BillingTest.java
@@ -11,47 +11,79 @@ class BillingTest {
 		Product myProduct = new Product("flacon de parfum", 10.0, false, "Beauty");
 		assertEquals(0.1 * myProduct.getPrice(), new TaxCalculatorClient().calculateTax(myProduct));
 	}
-	
+
 	@Test
 	void whenProductIsImportedAndNotFoodNorMedicineNorBookThenTaxIsFifteenPercent() {
 		Product myProduct = new Product("flacon de parfum", 10.0, true, "Beauty");
 		assertEquals(0.15 * myProduct.getPrice(), new TaxCalculatorClient().calculateTax(myProduct));
 	}
-	
+
 	@Test
 	void whenProductIsFoodNotImportedThenTaxIsZero() {
 		Product myProduct = new Product("boite de chocolats", 10.0, false, "Food");
 		assertEquals(0, new TaxCalculatorClient().calculateTax(myProduct));
 	}
-	
+
 	@Test
 	void whenProductIsMedicineNotImportedThenTaxIsZero() {
 		Product myProduct = new Product("boite de pilule contre la migraine", 10.0, false, "Medicine");
 		assertEquals(0, new TaxCalculatorClient().calculateTax(myProduct));
 	}
-	
+
 	@Test
 	void whenProductIsBookNotImportedThenTaxIsZero() {
 		Product myProduct = new Product("Le Clean Code pour les nuls", 10.0, false, "Book");
 		assertEquals(0, new TaxCalculatorClient().calculateTax(myProduct));
 	}
-	
+
 	@Test
 	void whenProductIsImportedFoodThenTaxIsFivePercent() {
 		Product myProduct = new Product("boite de chocolats", 10.0, true, "Food");
 		assertEquals(0.05 * myProduct.getPrice(), new TaxCalculatorClient().calculateTax(myProduct));
 	}
-	
+
 	@Test
 	void whenProductIsImportedMedicineThenTaxIsFivePercent() {
 		Product myProduct = new Product("boite de pilule contre la migraine", 10.0, true, "Medicine");
 		assertEquals(0.05 * myProduct.getPrice(), new TaxCalculatorClient().calculateTax(myProduct));
 	}
-	
+
 	@Test
 	void whenProductIsImportedBookThenTaxIsFivePercent() {
 		Product myProduct = new Product("Le Clean Code pour les nuls", 10.0, true, "Book");
 		assertEquals(0.05 * myProduct.getPrice(), new TaxCalculatorClient().calculateTax(myProduct));
 	}
+	
+	@Test
+	void whenNotRoundedTaxIs4_1985ThenRoundedTaxIs4_2() {
+		Product myProduct = new Product("flacon de parfum", 27.99, true, "Beauty");
+		assertEquals(4.2, new TaxCalculatorClient().calculateRoundedTax(myProduct));
+	}
+	
+	@Test
+	void whenNotRoundedTaxIs5_555ThenRoundedTaxIs5_6() {
+		Product myProduct = new Product("Le Clean Code pour les nuls vol.2", 11.11, true, "Book");
+		assertEquals(0.6, new TaxCalculatorClient().calculateRoundedTax(myProduct));
+	}
+	
+	@Test
+	void whenNotRoundedTaxIs0_5ThenRoundedTaxIs0_5() {
+		Product myProduct = new Product("Le Clean Code pour les nuls", 10.0, true, "Book");
+		assertEquals(0.5, new TaxCalculatorClient().calculateRoundedTax(myProduct));
+	}
+	
+	// 4.1985 + 1.899 + 0 + 0.5625
+	@Test
+	void whenProducsAreOneImportedPerfumeOnePerfumeOneHeachacheMedicineBoxOneImportedChocolateBoxThenTheTotalTaxIs6_70() {
+		Product myProduct1 = new Product("flacon de parfum", 27.99, true, "Beauty");
+		Product myProduct2 = new Product("flacon de parfum", 18.99, false, "Beauty");
+		Product myProduct3 = new Product("boite de pilule contre la migraine", 9.75, false, "Medicine");
+		Product myProduct4 = new Product("boite de chocolats", 11.25, true, "Food");
+		assertEquals(6.70, new TaxCalculatorClient().calculateRoundedTax(myProduct1)
+						 + new TaxCalculatorClient().calculateRoundedTax(myProduct2)
+						 + new TaxCalculatorClient().calculateRoundedTax(myProduct3)
+					     + new TaxCalculatorClient().calculateRoundedTax(myProduct4));
+	} 
+	
 
 }
--- src/billing/TaxCalculatorClient.java
+++ src/billing/TaxCalculatorClient.java
@@ -1,23 +1,62 @@
 package billing;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class TaxCalculatorClient {
 
 	public double calculateTax(Product product) {
 		return getAppropriateTaxSystem(product).getTax(product);
 	}
+
+//	public double calculateRoundedTax(Product product) {
+//		double initialTax = getAppropriateTaxSystem(product).getTax(product);
+//		
+//		BigDecimal initialTaxBigDecimal = new BigDecimal(String.valueOf(initialTax));
+//		double truncatedTax = (initialTaxBigDecimal.setScale(2, RoundingMode.FLOOR).doubleValue());
+//		String truncatedTaxString = String.valueOf(truncatedTax);
+//		String decimalPartOfTaxString = truncatedTaxString.substring(truncatedTaxString.indexOf('.') + 1);
+//		int decimalPartOfTax = Integer.valueOf(decimalPartOfTaxString);
+//
+//		if (!divisibleBy5(decimalPartOfTax)) {
+//			int resultOfDivision = (decimalPartOfTax / 5);
+//			double roundedTax = truncatedTax - (decimalPartOfTax * 0.01) + ((resultOfDivision + 1) * 0.05);
+//			return roundedTax;	
+//		}
+//		
+//		if (divisibleBy5(decimalPartOfTax) && truncatedTaxIsInferiorToInitialTax(truncatedTax, initialTax)) {
+//			double roundedTax = truncatedTax + 0.05;
+//			return roundedTax;
+//		}
+//		
+//		
+//		return initialTax;
+//	}
 	
-	TaxSystem getAppropriateTaxSystem(Product myProduct) {
+	public double calculateRoundedTax(Product product) {
+		double initialTax = getAppropriateTaxSystem(product).getTax(product);
+		double roundedTax = Math.ceil(initialTax / 0.05) / 20;
 		
+		return roundedTax;
+	}
+
+	TaxSystem getAppropriateTaxSystem(Product myProduct) {
+
 		if (productHasNoVATAndIsImported(myProduct)) {
 			return TaxSystem.IMPORTED_NON_TAXABLE;
 		}
-		if(productHasNoVAT(myProduct)) {
-			return TaxSystem.NON_TAXABLE;			
+		if (productHasNoVAT(myProduct)) {
+			return TaxSystem.NON_TAXABLE;
 		}
-		if(productIsImported(myProduct)) {
+		if (productIsImported(myProduct)) {
 			return TaxSystem.IMPORTED_TAXABLE;
 		}
 		return TaxSystem.TAXABLE;
+	}
+
+	Double getRoundedTax(Product myProduct) {
+		Double tax = getAppropriateTaxSystem(myProduct).getTax(myProduct);
+		return tax;
 	}
 
 	private boolean productHasNoVATAndIsImported(Product myProduct) {
@@ -42,5 +81,13 @@ public class TaxCalculatorClient {
 
 	private boolean productIsFood(Product myProduct) {
 		return myProduct.getCategory() == "Food";
-	}	
+	}
+
+	private boolean divisibleBy5(int decimalPart) {
+		return decimalPart % 5 == 0;
+	}
+	
+	private boolean truncatedTaxIsInferiorToInitialTax(double truncatedTax, double initialTax) {
+		return truncatedTax < initialTax;
+	}
 }
